3c860e528925deb1b1211c91b682c16e232ca022
cartheur <cartheur@pm.me>
2025-05-12 16:57:17 +0200

    Clear

--- CSCore.Ffmpeg/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("5de75590-74ff-4c8c-bb0c-26164ed01e3d")]
